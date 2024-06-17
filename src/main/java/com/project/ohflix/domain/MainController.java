54ab67814383362a48f48e2719a5d298da54dbf2
yunchocopie <yunnn687@naver.com>
2024-06-17 16:03:27 +0900

    account-membership 페이지 완료-ã ¡- merge

--- src/main/java/com/project/ohflix/domain/MainController.java
+++ src/main/java/com/project/ohflix/domain/MainController.java
@@ -7,38 +7,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/11")
-    public String getLoginForm(){
-        return "login-form";
-    }
-    @GetMapping("/12")
-    public String getPaymethodRegisterForm(){
-        return "paymethod-form";
-    }
-    @GetMapping("/13")
-    public String getPaymethodManage(){return "paymethod-manage";}
-
-    @GetMapping("/21")
-    public String getPasswordChangeForm(){return "password-change-form";}
-
     @GetMapping("/")
-    public String getMainPage(){
+    public String getMainPage() {
         return "main-page";
     }
-    @GetMapping("/1")
-    public String getAccountPage(){return "account-view";}
-    @GetMapping("/3")
-    public String getContentDetails(){
-        return "content-details";
-    }
-    @GetMapping("/5")
-    public String getProfileView(){
-        return "profile-form";
+
+    @GetMapping("/11")
+    public String getLoginForm() {
+        return "login-form";
     }
 
+    @GetMapping("/12")
+    public String getPaymethodRegisterForm() {
+        return "paymethod-form";
+    }
+
+    @GetMapping("/13")
+    public String getPaymethodManage() {
+        return "paymethod-manage";
+    }
+
+    @GetMapping("/21")
+    public String getPasswordChangeForm() {
+        return "password-change-form";
+    }
+
+    // account
     @GetMapping("/50")
+    public String getAccountPage() {
+        return "account-view";
+    }
+
+    // account
+    @GetMapping("/51")
     public String accountMembership() {
         return "account-membership";
     }
 
+    @GetMapping("/3")
+    public String getContentDetails() {
+        return "content-details";
+    }
+
+    @GetMapping("/5")
+    public String getProfileView() {
+        return "profile-form";
+    }
 }
